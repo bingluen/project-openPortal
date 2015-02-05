ea109d6f3abe44c9344dc06f159a9617518990c4
erickson-makotoki <a820224@gmail.com>
2015-02-05 09:37:20 +0800

    finish ^_^

--- python/getHomework.py
+++ python/getHomework.py
@@ -1,37 +1,166 @@
-username = 's1010541'
-password = '09er33ic88ks27on24'
-
-
+# coding=UTF-8
 import requests
+from bs4 import BeautifulSoup
 import re
+
+URL_LOGIN = 'https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx'
+URL_PORTAL_HOMEPAGE = 'https://portalx.yzu.edu.tw/PortalSocialVB/FMain/DefaultPage.aspx?Menu=Default'
+URL_STUDY_HISTORY = 'https://portalx.yzu.edu.tw/PortalSocialVB/FMain/PageByDuty.aspx?DutyType=std'
+URL_FIRST_TO_PAGE = 'https://portalx.yzu.edu.tw/PortalSocialVB/FPage/FirstToPage.aspx?PageID='
+URL_HOMEWORK_PAGE = 'https://portalx.yzu.edu.tw/PortalSocialVB/THom/HomeworkList.aspx?Menu=Hom'
+URL_DOWNLOAD_ATTACH = 'https://portalx.yzu.edu.tw/PortalSocialVB/File_DownLoad_Wk_zip.aspx?'
 
 r = requests.Session()
 
-content = r.post('https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx').text
+
+def writeFile(filename, content):
+    f = open(filename, 'w')
+    f.write(content)
+    f.close()
+
+class catchHomework:
+
+    def __init__ (self, username, password):
+        content = r.post(URL_LOGIN).text
+        ecoVIEWSTATEGENERATOR = re.findall('__VIEWSTATEGENERATOR id="__VIEWSTATEGENERATOR" value="([^"]*)"', content, re.S)
+        ecoVIEWSTATE = re.findall('__VIEWSTATE" id="__VIEWSTATE" value="([^"]*)"', content, re.S)
+        ecoEVENTVALIDATION = re.findall('__EVENTVALIDATION" id="__EVENTVALIDATION" value="([^"]*)"', content, re.S)
+        self.postdata={
+                '__VIEWSTATE':ecoVIEWSTATE,
+                '__VIEWSTATEGENERATOR':ecoEVENTVALIDATION,
+                '__EVENTVALIDATION':ecoEVENTVALIDATION,
+                'Txt_UserID':username,
+                'Txt_Password':password,
+                'ibnSubmit':'\xe7\x99\xbb\xe5\x85\xa5'
+                }
+        ### do login
+        login_result = r.post(URL_LOGIN, data=self.postdata, verify=False)
+
+        ### connect portal homepage after login (because set cookie)
+        content = r.get(URL_PORTAL_HOMEPAGE).text
+
+        ### init data structure
+        ### item of course list is
+        """
+        year
+        semester
+        course code
+        course name
+        """
+        self.courseList = []
 
 
-ecoVIEWSTATEGENERATOR = re.findall('__VIEWSTATEGENERATOR id="__VIEWSTATEGENERATOR" value="([^"]*)"', content, re.S)
-ecoVIEWSTATE = re.findall('__VIEWSTATE" id="__VIEWSTATE" value="([^"]*)"', content, re.S)
-ecoEVENTVALIDATION = re.findall('__EVENTVALIDATION" id="__EVENTVALIDATION" value="([^"]*)"', content, re.S)
+    def getCourseHistory(self):
+        ###get all of course
+        content = BeautifulSoup(r.get(URL_STUDY_HISTORY).text, 'lxml')
+        isFirst = True
+        for course in content.find('div', id='divDutyCoursePage').table.find_all('tr'):
+            if(isFirst):
+                isFirst = False
+                continue
+            row = course.find_all('td')
+            courseDetail = {}
+            courseDetail['year'] = row[0].text.strip(' ')
+            courseDetail['semester'] = row[1].text.strip(' ')
+            courseDetail['code'] = row[2].text.strip(' ')
+            courseDetail['pageID'] = re.findall('PageID=([0-9]+)', row[4].a['href'], re.S)[0]
+            self.courseList.append(courseDetail.copy())
 
-postdata={
-    '__VIEWSTATE':ecoVIEWSTATE,
-    '__VIEWSTATEGENERATOR':ecoEVENTVALIDATION,
-    '__EVENTVALIDATION':ecoEVENTVALIDATION,
-    'Txt_UserID':username,
-    'Txt_Password':password,
-    'ibnSubmit':'\xe7\x99\xbb\xe5\x85\xa5'
-}
+    def getHomework(self, pageID):
+        ###connect FirstToPage (because set cookie)
+        r.get(URL_FIRST_TO_PAGE+pageID)
+        content = BeautifulSoup(r.get(URL_HOMEWORK_PAGE).text, 'lxml').find('table', id='Table1').find_all('tr')
+        isFirst = True
+        isFinishGetRecord = True
+
+        ### Homework item column is
+        """
+        first row
+
+        node_index        value        type
+        0                 index       number
+        1                Schedule     string
+        2                 title       string
+        3         teacherUploadFile      MIX
+        4                deadline     string
+        5         studentUploadFileList  MIX
+        6        empty
+        7                isGroup      string(個人/？)
+        8              freeToSubmit   character(Y/N)
+        9                  grade      number
+        10                 comment    string
+
+        second row
+
+        0             descriptionOfHomework
 
 
-login_result = r.post('https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx', data=postdata, verify=False).text
+        """
 
-###get portal homepage after login
-content = r.get('https://portalx.yzu.edu.tw/PortalSocialVB/FMain/DefaultPage.aspx?Menu=Default').text
+        homeworkList = []
+        homeworkItem = {}
+        for row in content:
+
+            tds = row.find_all('td', colspan='11')
+            if len(tds) > 0:
+                continue
+            tds = row.find_all('td')
+
+            ### skip first row (title row)
+            if(isFirst):
+                isFirst = False
+                continue
+
+            ### mark row number is first row of a record or not
+            if(isFinishGetRecord):
+                isFinishGetRecord = False
+                ### first row of a record
+                homeworkItem['schedule'] = tds[1].text
+                homeworkItem['title'] = tds[2].text
+                homeworkItem['attach'] = self.parseAttach(tds[3])
+                homeworkItem['deadline'] = tds[4].text
+                homeworkItem['uploadFile'] = self.parseUploadFile(tds[5])
+                homeworkItem['isGroup'] = False if tds[7].text == u'個人' else True
+                homeworkItem['freeSubmit'] = False if tds[8].text == 'N' else True
+                homeworkItem['grade'] = tds[9].text
+                homeworkItem['comment'] = tds[10].text
 
 
-###get all of course
+            else:
+                isFinishGetRecord = True
+                homeworkItem['description'] = tds[0].text
+                homeworkList.append(homeworkItem.copy())
+                homeworkItem.clear()
 
-content = r.get('https://portalx.yzu.edu.tw/PortalSocialVB/FMain/PageByDuty.aspx?DutyType=std').text
+        return homeworkList
 
-print(content)
+    def parseAttach(self, attach):
+        if len(attach.contents) > 0:
+            attachDetail = {}
+            parameter = re.findall('type=([0-9])&File_name=(.*)&id=([0-9]+)', attach.contents[0]['href'], re.S)
+            attachDetail['type'] = parameter[0][0]
+            attachDetail['filename'] = parameter[0][1]
+            attachDetail['id'] = parameter[0][2]
+
+            return attachDetail
+
+    def parseUploadFile(self, upload):
+        if len(upload.contents) > 0:
+            uploadDetail = {}
+
+            parameter = re.findall('File_name=(.*)&type=([0-9])&id=([0-9]+)', upload.a['href'], re.S)
+            uploadDetail['filename'] = parameter[0][0]
+            uploadDetail['type'] = parameter[0][1]
+            uploadDetail['id'] = parameter[0][2]
+
+            return uploadDetail
+
+    def getAttach(self, attach):
+        download = r.get(URL_DOWNLOAD_ATTACH+'File_name='+attach['filename']+'&id='+attach['id']+'&type='+attach['type'])
+        writeFile(attach['filename'], download.content)
+
+
+
+
+
+parser = catchHomework('', '')
