f730bd39275fd848bb6da18c7e662a45f00d8e41
Erickson Juang <a820224@gmail.com>
2015-08-30 04:40:20 +0800

    modify navbar

--- nodejs/component/navbar.jsx
+++ nodejs/component/navbar.jsx
@@ -1,9 +1,9 @@
 var Navbar = React.createClass({
   render: function() {
     return (
-      <div className="ui inverted stackable menu">
-        <div className="ui container">
-          <div className="header item">選課模擬系統</div>
+      <div className="ui top fixed menu">
+        <div className="ui container content">
+          <div className="item">選課模擬系統</div>
         </div>
       </div>
     )
